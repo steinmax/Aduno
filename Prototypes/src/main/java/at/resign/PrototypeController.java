1c17c1759f4e1aa8178018edbd3cf13ffdf1c77f
Simon RS <simon.rauschschott@gmail.com>
2021-12-06 15:17:06 +0100

    #3 add current date "simulation"

--- Prototypes/src/main/java/at/resign/PrototypeController.java
+++ Prototypes/src/main/java/at/resign/PrototypeController.java
@@ -5,14 +5,18 @@ import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class PrototypeController implements Initializable {
     public Label lblRoomNumber;
+    public Label lblDate;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         lblRoomNumber.setText(App.getRoomNumber());
+        lblDate.setText(LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
     }
 
     public void btn_dakrmodeToggle_click(MouseEvent mouseEvent) {
