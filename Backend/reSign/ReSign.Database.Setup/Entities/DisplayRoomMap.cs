e17d2da0997302e9f4de2bc046dbf0af7b4d453f
Simon Rausch-Schott <simon.rauschschott@gmail.com>
2022-01-14 19:25:35 +0100

    Add navigation properties

--- Backend/reSign/ReSign.Database.Setup/Entities/DisplayRoomMap.cs
+++ Backend/reSign/ReSign.Database.Setup/Entities/DisplayRoomMap.cs
@@ -4,5 +4,9 @@
     {
         public int DisplayId { get; set; }
         public int RoomId { get; set; }
+
+        //Navigation
+        public Display Display { get; set; }
+        public Room Room { get; set; }
     }
 }
