60c781406f7355eac3222390e5c6768f35dbb450
Simon Rausch-Schott <simon.rauschschott@gmail.com>
2022-01-14 19:25:57 +0100

    Add DbSets to DbContext

--- Backend/reSign/ReSign.Database.Setup/DataContext/BaseDbContext.cs
+++ Backend/reSign/ReSign.Database.Setup/DataContext/BaseDbContext.cs
@@ -7,8 +7,21 @@ using System.Threading.Tasks;
 
 namespace ReSign.Database.Logic.DataContext
 {
-    protected class BaseDbContext : DbContext
+    public class BaseDbContext : DbContext
     {
+        # region Testing purposes only:
+        private static string ConnectStr => "";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        #endregion
+        public DbSet<Entities.Display> DisplaySet { get; set; }
+        public DbSet<Entities.Room> RoomSet { get; set; }
+        public DbSet<Entities.DisplayRoomMap> DisplayRoomMapSet { get; set; }
 
     }
 }
