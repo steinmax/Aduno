04ee3d23e68709947d04916229b935530cab9f2f
Simon Rausch-Schott <simon.rausch-schott@24speed.at>
2022-01-16 12:52:38 +0100

    Add testing code

--- Backend/reSign/ReSign.Database.ConApp/Program.cs
+++ Backend/reSign/ReSign.Database.ConApp/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using ReSign.Database.Logic.DataContext;
+
+Console.WriteLine("Hello");
+
+using var context = new BaseDbContext();
+context.RoomSet.ToList().ForEach(r => global::System.Console.WriteLine(r));
