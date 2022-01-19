32db716b3528a0a1e3dbf84ad0ac25a7c8de2538
Simon Rausch-Schott <simon.rausch-schott@24speed.at>
2022-01-19 13:01:06 +0100

    Add Room reference

--- Backend/reSign/ReSign.Database.Logic/Entities/General/Class.cs
+++ Backend/reSign/ReSign.Database.Logic/Entities/General/Class.cs
@@ -6,7 +6,9 @@ public class Class : VersionObject
 {
     public string Designation { get; set; }
     public int Grade { get; set; }
+    public int RoomId { get; set; }
 
     //Navigation
     public List<Pupil> Pupils { get; set; } = new();
+    public Room Room { get; set; }
 }
