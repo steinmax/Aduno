8dbbbed6fa82fd45a6eb7192e39882232aa9b70b
Simon Rausch-Schott <simon.rausch-schott@24speed.at>
2022-01-19 13:00:50 +0100

    Add Entity QRSessionCookie

---
+++ Backend/reSign/ReSign.Database.Logic/Entities/PresenceSystem/QRSessionCookie.cs
@@ -0,0 +1,11 @@
+﻿using ReSign.Database.Logic.Entities.Base;
+
+namespace ReSign.Database.Logic.Entities.PresenceSystem
+{
+    public class QRSessionCookie : VersionObject
+    {
+        public string Token { get; set; } = string.Empty;
+        public DateTime DateCreated { get; set; }
+        public bool IsValid { get; set; }
+    }
+}
