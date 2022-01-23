3add94d68228dc601b905271e04c8f3b57b6bd51
Simon Rausch-Schott <simon.rauschschott@gmail.com>
2022-01-23 22:53:01 +0100

    Cleanup Program.cs

--- Backend/reSign/ReSign.Database.ConApp/Program.cs
+++ Backend/reSign/ReSign.Database.ConApp/Program.cs
@@ -1,6 +1,2 @@
-﻿using ReSign.Database.Logic.DataContext;
+﻿Console.WriteLine("Hello");
 
-Console.WriteLine("Hello");
-
-using var context = new ReSignDbContext();
-context.RoomSet.ToList().ForEach(r => Console.WriteLine(r));
