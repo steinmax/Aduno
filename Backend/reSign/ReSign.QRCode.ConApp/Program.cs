6bde3756c1af811a099feab8877b4e1abcae23cb
Simon Rausch-Schott <simon.rauschschott@gmail.com>
2022-02-08 18:00:23 +0100

    Add QrCode console app with example

---
+++ Backend/reSign/ReSign.QRCode.ConApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using ReSign.QRCode.Logic;
+
+Console.WriteLine("ReSign QrCode Generator |");
+Console.WriteLine("------------------------+");
+
+
+var task = QRCodeGenerator.GeneratePngFileAsync("qrcode.png", "https://resign.byiconic.at/dev", logoFilePath: "reSignLogo.png");
+Console.WriteLine("QrCode generation started...");
+task.Wait();
+
+Console.WriteLine("QrCode generation finished!");
