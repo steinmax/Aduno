5ffa92b8b665fac26d76a3b6975e598639c780ff
Simon Rausch-Schott <simon.rauschschott@gmail.com>
2022-02-08 20:52:08 +0100

    Add 2 tests for QRCodeGenerator

--- Backend/reSign/ReSign.QRCode.Logic.UnitTest/QRCodeGeneratorTests.cs
+++ Backend/reSign/ReSign.QRCode.Logic.UnitTest/QRCodeGeneratorTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace ReSign.QRCode.Logic.UnitTest
 {
@@ -6,10 +7,32 @@ namespace ReSign.QRCode.Logic.UnitTest
     public class QRCodeGeneratorTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CreateQRCode_ResignByiconicAt_ExpectNonNullByteArray()
         {
-            //ToDo
+            var qrCode = QRCodeGenerator.GeneratePngBytesAsync("https://resign.byiconic.at")
+                .Result;
+            Assert.IsNotNull(qrCode);
+        }
 
+        [TestMethod]
+        public void CreateQRCodePng_ResignByiconicAt_ExpectOutputFile()
+        {
+            const string outputFile = "test2_qr_output.png";
+            //Delete File if already exists
+            if(File.Exists(outputFile))
+                File.Delete(outputFile);
+
+            //Generate new file/qrcode
+            QRCodeGenerator.GeneratePngFileAsync(outputFile, "https://resign.byiconic.at")
+                .Wait();
+
+            //Check if new file is created
+            var exists = File.Exists(outputFile);
+            Assert.IsTrue(exists);
+
+            var sizeInBytes = File.ReadAllBytes(outputFile).Length;
+            var sizeBiggerThan0 = sizeInBytes > 0;
+            Assert.IsTrue(sizeBiggerThan0);
         }
     }
 }
