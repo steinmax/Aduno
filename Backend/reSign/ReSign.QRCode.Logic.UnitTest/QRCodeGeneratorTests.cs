1c87b8eb944056652cc2ba5757db942dd99c5562
Simon Rausch-Schott <simon.rauschschott@gmail.com>
2022-02-08 18:04:20 +0100

    Add QrCode UnitTest project

---
+++ Backend/reSign/ReSign.QRCode.Logic.UnitTest/QRCodeGeneratorTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReSign.QRCode.Logic.UnitTest
+{
+    [TestClass]
+    public class QRCodeGeneratorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //ToDo
+
+        }
+    }
+}
