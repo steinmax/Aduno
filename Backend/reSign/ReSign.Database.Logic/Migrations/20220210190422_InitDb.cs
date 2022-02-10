a278de31d9c1c724e53f28e2f3ae7093f39b6afd
Simon Rausch-Schott <simon.rauschschott@gmail.com>
2022-02-10 20:05:14 +0100

    Add migration and update database

---
+++ Backend/reSign/ReSign.Database.Logic/Migrations/20220210190422_InitDb.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ReSign.Database.Logic.Migrations
+{
+    public partial class InitDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Display",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RowVersion = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Display", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Organisation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organisation", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Room",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Designation = table.Column<string>(type: "text", nullable: false),
+                    Floor = table.Column<int>(type: "integer", nullable: false),
+                    RoomNumber = table.Column<int>(type: "integer", nullable: false),
+                    Notes = table.Column<string>(type: "text", nullable: true),
+                    OranisationId = table.Column<int>(type: "integer", nullable: false),
+                    OrganisationId = table.Column<int>(type: "integer", nullable: true),
+                    RowVersion = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Room", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Room_Organisation_OrganisationId",
+                        column: x => x.OrganisationId,
+                        principalTable: "Organisation",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    OrganisationId = table.Column<int>(type: "integer", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_User_Organisation_OrganisationId",
+                        column: x => x.OrganisationId,
+                        principalTable: "Organisation",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Device",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UniqueId = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Device", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Device_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Timestamp",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    RoomId = table.Column<int>(type: "integer", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Timestamp", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Timestamp_Room_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Room",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Timestamp_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QRToken",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Token = table.Column<string>(type: "text", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsValid = table.Column<bool>(type: "boolean", nullable: false),
+                    UsedByDeviceId = table.Column<int>(type: "integer", nullable: true),
+                    DisplayId = table.Column<int>(type: "integer", nullable: true),
+                    RowVersion = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QRToken", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QRToken_Device_UsedByDeviceId",
+                        column: x => x.UsedByDeviceId,
+                        principalTable: "Device",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_QRToken_Display_DisplayId",
+                        column: x => x.DisplayId,
+                        principalTable: "Display",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Device_UniqueId",
+                table: "Device",
+                column: "UniqueId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Device_UserId",
+                table: "Device",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Organisation_Name",
+                table: "Organisation",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QRToken_DisplayId",
+                table: "QRToken",
+                column: "DisplayId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QRToken_Token",
+                table: "QRToken",
+                column: "Token",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QRToken_UsedByDeviceId",
+                table: "QRToken",
+                column: "UsedByDeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Room_OrganisationId",
+                table: "Room",
+                column: "OrganisationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Timestamp_RoomId",
+                table: "Timestamp",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Timestamp_UserId",
+                table: "Timestamp",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_FirstName_LastName_OrganisationId",
+                table: "User",
+                columns: new[] { "FirstName", "LastName", "OrganisationId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_OrganisationId",
+                table: "User",
+                column: "OrganisationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QRToken");
+
+            migrationBuilder.DropTable(
+                name: "Timestamp");
+
+            migrationBuilder.DropTable(
+                name: "Device");
+
+            migrationBuilder.DropTable(
+                name: "Display");
+
+            migrationBuilder.DropTable(
+                name: "Room");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Organisation");
+        }
+    }
+}
