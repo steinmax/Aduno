15c9e5e2a27cdcf205a96faed0ea9a9605aebcc9
Simon Rausch-Schott <simon.rauschschott@gmail.com>
2022-02-10 19:45:01 +0100

    Rename "QRSessionCookie.cs" to "QRToken.cs"

---
+++ Backend/reSign/ReSign.Database.Logic/Entities/PresenceSystem/QRToken.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReSign.Database.Logic.Entities.Base;
+using ReSign.Database.Logic.Entities.General;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReSign.Database.Logic.Entities.PresenceSystem
+{
+    [Table("QRToken")]
+    [Index(nameof(Token), IsUnique = true)]
+    public class QRToken : VersionObject
+    {
+        [Required]
+        public string Token { get; set; } = string.Empty;
+        [Required]
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        [Required]
+        public bool IsValid { get; set; } = true;
+
+        [Column("UsedByDeviceId")]
+        public int? DeviceId { get; set; }
+        
+        public int? DisplayId { get; set; }
+
+     
+        //navigation
+        public Device? UsedByDevice { get; set; }
+        public Display? Display { get; set; }
+    }
+}
