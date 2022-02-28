9bd2436469a546ca0096bfc4e262edc64e2b02f5
steinmax <siegl.maxi@gmail.com>
2022-02-28 16:58:27 +0100

    fix bug in interactionController

--- Backend/reSign/ReSign.WebAPI/Controllers/InteractionController.cs
+++ Backend/reSign/ReSign.WebAPI/Controllers/InteractionController.cs
@@ -1,9 +1,12 @@
-﻿using ReSign.Database.Logic.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using ReSign.Database.Logic.Controllers;
 using ReSign.Database.Logic.Entities.PresenceSystem;
 using ReSign.WebAPI.Models;
 
 namespace ReSign.WebAPI.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class InteractionController : GenericController<Database.Logic.Entities.PresenceSystem.Interaction, InteractionEdit, InteractionModel>
     {
         public InteractionController(Database.Logic.Controllers.InteractionController controller) : base(controller)
