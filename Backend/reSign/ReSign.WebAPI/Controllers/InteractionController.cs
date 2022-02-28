c08152fb64cfc9f36448a892fe6752dd33a19df1
steinmax <siegl.maxi@gmail.com>
2022-02-28 16:55:20 +0100

    Add interaction-, edit and controller files

--- Backend/reSign/ReSign.WebAPI/Controllers/InteractionController.cs
+++ Backend/reSign/ReSign.WebAPI/Controllers/InteractionController.cs
@@ -1,6 +1,14 @@
-﻿namespace ReSign.WebAPI.Controllers
+﻿using ReSign.Database.Logic.Controllers;
+using ReSign.Database.Logic.Entities.PresenceSystem;
+using ReSign.WebAPI.Models;
+
+namespace ReSign.WebAPI.Controllers
 {
-    public class InteractionController
+    public class InteractionController : GenericController<Database.Logic.Entities.PresenceSystem.Interaction, InteractionEdit, InteractionModel>
     {
+        public InteractionController(Database.Logic.Controllers.InteractionController controller) : base(controller)
+        {
+        }
     }
 }
+ 
---
+++ Backend/reSign/ReSign.WebAPI/Models/InteractionEdit.cs
@@ -0,0 +1,12 @@
+﻿using ReSign.Database.Logic.Enumerations;
+
+namespace ReSign.WebAPI.Models
+{
+    public class InteractionEdit
+    {
+        public DateTime DateTime { get; set; }
+        public InteractionType Type { get; set; }
+        public int UserId { get; set; }
+        public int RoomId { get; set; }
+    }
+}
