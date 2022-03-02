21bc02189f631ee2829a424a795ce56069415b3e
Simon Rausch-Schott <simon.rausch-schott@24speed.at>
2022-03-02 12:46:10 +0100

    Add login function with WRONG FORMAT

--- Frontend/fcis-ui/logic.js
+++ Frontend/fcis-ui/logic.js
@@ -1,4 +1,3 @@
-const 
 
 function readFromLocalStorage(keyword) {
     let result = localStorage.getItem(keyword);
@@ -13,4 +12,27 @@ function writeToLocalStorage(keyword, value) {
     }
 
     localStorage.setItem(keyword, value);
+}
+
+async function login(username, password) {
+    const reqBody = {       //WRONG FORMAT; Finish Backend API to get correct format
+        "name": username,
+        "pass": password
+    };
+
+    const response = await fetch(url, {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(reqBody) // body data type must match "Content-Type" header
+      });
+
+    return response;
 }
