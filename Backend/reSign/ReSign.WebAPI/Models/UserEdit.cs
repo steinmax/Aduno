36d293f857525d34af400bedb7e4eae56d413392
steinmax <siegl.maxi@gmail.com>
2022-03-13 22:05:49 +0100

    Update UserEdit

--- Backend/reSign/ReSign.WebAPI/Models/UserEdit.cs
+++ Backend/reSign/ReSign.WebAPI/Models/UserEdit.cs
@@ -8,5 +8,6 @@ namespace ReSign.WebAPI.Models
         public string LastName { get; set; } = string.Empty;
         public Role Role { get; set; } = Role.Member;
         public int OrganisationId { get; set; }
+        public string Username { get; set; } = string.Empty;
     }
 }
