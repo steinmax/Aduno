6e67c95641fef91dd4f9942f44dfe8911506e95b
steinmax <siegl.maxi@gmail.com>
2022-03-13 22:05:36 +0100

    add credentials check mechanism

--- Backend/reSign/ReSign.Database.Logic/Controllers/UserController.cs
+++ Backend/reSign/ReSign.Database.Logic/Controllers/UserController.cs
@@ -13,5 +13,23 @@ namespace ReSign.Database.Logic.Controllers
         }
 
         //further BL
+
+        public async Task<User?> GetByNameAsync(string name)
+        {
+            var usr = await EntitySet.Where(e => e.Username == name).FirstOrDefaultAsync();
+            return usr;
+        }
+
+        public async Task<User?> CheckCredentials(string username, string password)
+        {
+            var usr = await GetByNameAsync(username);
+            if (usr == null)
+                return null;
+
+            if (usr.Password == password)
+                return usr;
+            else
+                return null;
+        }
     }
 }
