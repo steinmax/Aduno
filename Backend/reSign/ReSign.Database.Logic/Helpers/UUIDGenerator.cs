a2c58a83dbf0483aff440b0c7df5190307d11b74
Benjamin Imsirovic <b.imsirovic@HTBLALeonding.onmicrosoft.com>
2022-03-14 08:07:52 +0100

    QrToken-Generator einfügen

---
+++ Backend/reSign/ReSign.Database.Logic/Helpers/UUIDGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReSign.Database.Logic.Helpers
+{
+    public static class UUIDGenerator
+    {
+        public static string QrTokenGenerator()
+        {
+            Guid g = Guid.NewGuid();
+            string str = g.ToString();
+            string[] result = str.Split("-");
+            if (result != null)
+            {
+                return result.FirstOrDefault();
+            }
+            else
+            {
+                throw new ArgumentNullException("Es enstand ein Fehler bei der Tokengenerierung bitte versuchen Sie es nocht ein Mal");
+            }
+        }
+
+        public static string UserTokenGenerator()
+        {
+            Guid g = Guid.NewGuid();
+            string str = g.ToString();
+            return str;
+        }
+    }
+}
