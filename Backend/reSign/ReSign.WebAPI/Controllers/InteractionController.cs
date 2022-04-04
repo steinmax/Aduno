b3a12b1594b8f6637a965df14062a511442186df
steinmax <siegl.maxi@gmail.com>
2022-04-04 08:48:28 +0200

    add togglecheckstate
    ... for toggling the current state

--- Backend/reSign/ReSign.WebAPI/Controllers/InteractionController.cs
+++ Backend/reSign/ReSign.WebAPI/Controllers/InteractionController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ReSign.Database.Logic.Controllers;
 using ReSign.Database.Logic.Entities.PresenceSystem;
+using ReSign.Database.Logic.Enumerations;
 using ReSign.WebAPI.Models;
 
 namespace ReSign.WebAPI.Controllers
@@ -31,6 +32,31 @@ namespace ReSign.WebAPI.Controllers
                 throw new Exception("Controller null");
 
             return ToModel(await ctrl.GetLastInteraction(id));
+        }
+
+
+        [HttpPost("{id}/{roomId}/{qrtokenId}")]
+        public async Task<ActionResult<InteractionModel>> ToggleCheckState(int id, int roomId, int qrtokenId)
+        {
+            var ctrl = EntityController as Database.Logic.Controllers.InteractionController;
+
+            if (ctrl == null)
+                throw new Exception("Controller null");
+            
+            Interaction last = await ctrl.GetLastInteraction(id);
+
+            InteractionType type = last == null ? InteractionType.CheckIn : last.Type == InteractionType.CheckIn ? InteractionType.CheckOut : InteractionType.CheckIn;
+
+            Interaction interaction = new Interaction();
+            interaction.UserId = id;
+            interaction.RoomId = roomId;
+            interaction.QRTokenId = qrtokenId;
+            interaction.DateTime = DateTime.Now;
+            interaction.Type = type;
+
+            Interaction act = await ctrl.InsertAsync(interaction);
+
+            return ToModel(act);
         }
     }
 }
