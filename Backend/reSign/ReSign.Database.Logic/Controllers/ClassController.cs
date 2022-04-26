e5f1710214776d48edb03ba8488d3e2b9652fa57
Simon Rausch-Schott <simon.rausch-schott@24speed.at>
2022-04-26 11:27:34 +0200

    Add ClassController for Class entity

---
+++ Backend/reSign/ReSign.Database.Logic/Controllers/ClassController.cs
@@ -0,0 +1,18 @@
+﻿namespace ReSign.Database.Logic.Controllers
+{
+    public class ClassController : GenericController<Entities.Class>
+    {
+        public ClassController() : base()
+        {
+        }
+
+        public ClassController(ControllerObject other) : base(other)
+        {
+        }
+
+
+        //further business logic
+
+
+    }
+}
