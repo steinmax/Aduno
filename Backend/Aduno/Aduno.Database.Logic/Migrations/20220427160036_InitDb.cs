3093030337b60497448769c9a2639895ff75dce6
Simon Rausch-Schott <simon.rauschschott@gmail.com>
2022-04-27 18:22:18 +0200

    Add migrations and update database (new datamodel)

---
+++ Backend/Aduno/Aduno.Database.Logic/Migrations/20220427160036_InitDb.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Aduno.Database.Logic.Migrations
+{
+    public partial class InitDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ClassSet",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClassSet", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Room",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Designation = table.Column<string>(type: "text", nullable: false),
+                    Floor = table.Column<int>(type: "integer", nullable: false),
+                    RoomNumber = table.Column<int>(type: "integer", nullable: false),
+                    Notes = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Room", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    ClassId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_User_ClassSet_ClassId",
+                        column: x => x.ClassId,
+                        principalTable: "ClassSet",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Interaction",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    RoomId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interaction", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Interaction_Room_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Room",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Interaction_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Interaction_RoomId",
+                table: "Interaction",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Interaction_UserId",
+                table: "Interaction",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_ClassId",
+                table: "User",
+                column: "ClassId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_FirstName_LastName",
+                table: "User",
+                columns: new[] { "FirstName", "LastName" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Username",
+                table: "User",
+                column: "Username",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Interaction");
+
+            migrationBuilder.DropTable(
+                name: "Room");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "ClassSet");
+        }
+    }
+}
---
+++ Backend/Aduno/Aduno.Database.Logic/Migrations/20220427161144_RenameClassTableAddUniqueName.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aduno.Database.Logic.Migrations
+{
+    public partial class RenameClassTableAddUniqueName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_ClassSet_ClassId",
+                table: "User");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ClassSet",
+                table: "ClassSet");
+
+            migrationBuilder.RenameTable(
+                name: "ClassSet",
+                newName: "Class");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Class",
+                table: "Class",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Class_Name",
+                table: "Class",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_Class_ClassId",
+                table: "User",
+                column: "ClassId",
+                principalTable: "Class",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_Class_ClassId",
+                table: "User");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Class",
+                table: "Class");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Class_Name",
+                table: "Class");
+
+            migrationBuilder.RenameTable(
+                name: "Class",
+                newName: "ClassSet");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ClassSet",
+                table: "ClassSet",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_ClassSet_ClassId",
+                table: "User",
+                column: "ClassId",
+                principalTable: "ClassSet",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
