e332557a05bac5647fbb2066ca4eb40101389be4
Simon Rausch-Schott <simon.rauschschott@gmail.com>
2022-04-28 20:49:22 +0200

    Add migration with edited models

---
+++ Backend/Aduno/Aduno.Database.Logic/Migrations/20220428184654_EditWebApiModels.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aduno.Database.Logic.Migrations
+{
+    public partial class EditWebApiModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
