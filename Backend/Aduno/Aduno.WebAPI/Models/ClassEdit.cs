6e7af911408d94a1b9c5219e5231537cfe17303c
Simon Rausch-Schott <simon.rauschschott@gmail.com>
2022-04-28 21:33:42 +0200

    Add WebApi models for Class

---
+++ Backend/Aduno/Aduno.WebAPI/Models/ClassEdit.cs
@@ -0,0 +1,7 @@
+﻿namespace Aduno.WebAPI.Models
+{
+    public class ClassEdit
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ Backend/Aduno/Aduno.WebAPI/Models/ClassModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Aduno.WebAPI.Models
+{
+    public class ClassModel : IdentityModel
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
