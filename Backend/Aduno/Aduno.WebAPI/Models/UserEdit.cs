202febb77c03b5a203ac88b08d012df8138824e0
Simon Rausch-Schott <simon.rauschschott@gmail.com>
2022-04-28 21:08:23 +0200

    Add username to UserEdit Model

--- Backend/Aduno/Aduno.WebAPI/Models/UserEdit.cs
+++ Backend/Aduno/Aduno.WebAPI/Models/UserEdit.cs
@@ -4,6 +4,7 @@ namespace Aduno.WebAPI.Models
 {
     public class UserEdit
     {
+        public string Username { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public Role Role { get; set; } = Role.Member;
