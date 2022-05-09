d471fd214dab32e9e95e763bf63d8859af63105b
steinmax <siegl.maxi@gmail.com>
2022-05-09 11:40:00 +0200

    add jwt auth

--- Backend/Aduno/Aduno.WebAPI/Controllers/PresenceController.cs
+++ Backend/Aduno/Aduno.WebAPI/Controllers/PresenceController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 
 namespace Aduno.WebAPI.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class PresenceController : ControllerBase
