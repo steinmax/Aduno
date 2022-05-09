51da0ac96fb4ae07f0ea454b60586bdcc0664d34
steinmax <siegl.maxi@gmail.com>
2022-05-09 16:51:14 +0200

    add role from jwt

---
+++ Backend/Aduno/Aduno.WebAPI/Common.cs
@@ -0,0 +1,21 @@
+﻿using Aduno.Database.Logic.Enumerations;
+using System.Security.Claims;
+
+namespace Aduno.WebAPI
+{
+    public class Common
+    {
+        public static Role? GetUserRoleFromUser(ClaimsPrincipal user)
+        {
+            var claim = user.FindFirst("userrole");
+
+            if (claim == null)
+                return null;
+
+            var value = claim.Value;
+            Role role = (Role)Convert.ToInt32(value);
+
+            return role;
+        }
+    }
+}
