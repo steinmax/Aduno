da97d0a13b209655dd432d1fb2efa799378acd6b
steinmax <siegl.maxi@gmail.com>
2022-05-09 09:49:07 +0200

    add jwt auth

--- Backend/Aduno/Aduno.WebAPI/Program.cs
+++ Backend/Aduno/Aduno.WebAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +14,20 @@ builder.Services.AddTransient<Aduno.Database.Logic.Controllers.InteractionContro
 builder.Services.AddTransient<Aduno.Database.Logic.Controllers.RoomController>();
 builder.Services.AddTransient<Aduno.Database.Logic.Controllers.UserController>();
 builder.Services.AddTransient<Aduno.Database.Logic.Controllers.ClassController>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 
 var app = builder.Build();
 
@@ -24,6 +42,7 @@ var app = builder.Build();
 //Enable once we know how to run it in parallel to nginx (port 80)
 //app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
