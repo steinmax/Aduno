a0a390a5865f4e180af81ad5c69a7479c7b3c4e2
Simon Rausch-Schott <simon.rausch-schott@24speed.at>
2022-05-30 16:22:09 +0200

    Add migration

--- Backend/Aduno/Aduno.Database.Logic/Migrations/20220530135414_AddPotentialChanges.Designer.cs
+++ Backend/Aduno/Aduno.Database.Logic/Migrations/20220530135414_AddPotentialChanges.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Aduno.Database.Logic.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Aduno.Database.Logic.Migrations
 {
     [DbContext(typeof(AdunoDbContext))]
-    partial class AdunoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220530135414_AddPotentialChanges")]
+    partial class AddPotentialChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
